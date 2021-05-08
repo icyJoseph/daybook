3c3ee7c5f7a38f18cab1c091601e12fda4c2bdc6
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-05-08 23:07:25 +0200

    feat: Handle missing refresh token case

--- web-app/src/pages/api/search/bulk.ts
+++ web-app/src/pages/api/search/bulk.ts
@@ -1,24 +1,28 @@
 import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function search(req: NextApiRequest, res: NextApiResponse) {
+async function bulk(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") return res.status(401).json({ statusCode: 401 });
 
-  const session = await getAccessToken(req, res);
-
-  if (!session) return res.status(401).json({ statusCode: 401 });
-
-  const { accessToken } = session;
-
   try {
+    const session = await getAccessToken(req, res);
+
+    if (!session) return res.status(401).json({ statusCode: 401 });
+
+    const { accessToken } = session;
+
     const data = await fetch(process.env.PROXY_URL, {
       headers: { Authorization: `Bearer ${accessToken}` }
     }).then((res) => res.json());
 
     return res.json(data);
   } catch (err) {
+    if ("code" in err) {
+      if (err.code === "access_token_expired")
+        return res.redirect("/logout").json({ statusCode: 302 });
+    }
     return res.status(500).json({ statusCode: 500 });
   }
 }
 
-export default withApiAuthRequired(search);
+export default withApiAuthRequired(bulk);
--- web-app/src/pages/api/search/query.ts
+++ web-app/src/pages/api/search/query.ts
@@ -1,18 +1,18 @@
 import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function search(req: NextApiRequest, res: NextApiResponse) {
+async function query(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") return res.status(401).json({ statusCode: 401 });
 
-  const session = await getAccessToken(req, res);
-
-  if (!session) return res.status(401).json({ statusCode: 401 });
-
-  const { accessToken } = session;
-
-  const { query } = req;
-
   try {
+    const session = await getAccessToken(req, res);
+
+    if (!session) return res.status(401).json({ statusCode: 401 });
+
+    const { accessToken } = session;
+
+    const { query } = req;
+
     const data = await fetch(
       `${process.env.PROXY_URL}/search?query=${query.term}`,
       {
@@ -22,8 +22,12 @@ async function search(req: NextApiRequest, res: NextApiResponse) {
 
     return res.json(data);
   } catch (err) {
+    if ("code" in err) {
+      if (err.code === "access_token_expired")
+        return res.redirect("/logout").json({ statusCode: 302 });
+    }
     return res.status(500).json({ statusCode: 500 });
   }
 }
 
-export default withApiAuthRequired(search);
+export default withApiAuthRequired(query);
