bbbe1e2299ffe62ab197123f0b281ae33cdf9497
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-05-13 20:09:42 +0200

    fix: Include protocol on URL

--- web-app/src/utils/auth0.ts
+++ web-app/src/utils/auth0.ts
@@ -2,7 +2,9 @@ import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export default initAuth0({
   secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.VERCEL_URL,
+  baseURL: process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000",
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET
