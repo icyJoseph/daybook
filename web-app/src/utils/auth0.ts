1c2f4565cf5d0d54aa7f039007a9014e36342f9e
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-05-13 20:20:36 +0200

    chore: use AUTH0_BASE_URL if present, otherwise use VERCEL_URL

--- web-app/src/utils/auth0.ts
+++ web-app/src/utils/auth0.ts
@@ -2,9 +2,7 @@ import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export default initAuth0({
   secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000",
+  baseURL: process.env.AUTH0_BASE_URL || `https://${process.env.VERCEL_URL}`,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET
